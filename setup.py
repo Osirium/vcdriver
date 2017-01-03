0fb589a4d21f586c03f8b1d40d3373f51ea62bc7
Carlos <carlos.ruiz.lantero@gmail.com>
2017-01-03 14:05:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(
-    name='vcenter-driver',
     version='0.1.0',
+    name='vcenter-driver',
     description='Vcenter driver for testing purposes',
+    long_description=readme(),
+    url='https://github.com/Lantero/vcenter-driver',
     author='Carlos Ruiz Lantero',
     author_email='carlos.ruiz.lantero@gmail.com',
-    install_requires=['fabric', 'pyvmomi==6.5'],
+    license='MIT',
+    install_requires=['fabric', 'pyvmomi'],
     packages=find_packages(),
+    test_suite='nose.collector',
+    tests_require=['nose'],
 )
