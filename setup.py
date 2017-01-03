2fe8bb675be8545f548da5d6159e61030d938278
Carlos <carlos.ruiz.lantero@gmail.com>
2017-01-03 14:23:09 +0000

    Add requirements stuff

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
+import os
 from setuptools import setup, find_packages
 
 
 def readme():
     with open('README.md') as f:
         return f.read()
+
+
+def src_requirements():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+
+def test_requirements():
+    with open(os.path.join('test', 'requirements.txt')) as f:
+        return f.read().splitlines()
+
 
 setup(
     version='0.1.0',
@@ -14,8 +26,8 @@ setup(
     author='Carlos Ruiz Lantero',
     author_email='carlos.ruiz.lantero@gmail.com',
     license='MIT',
-    install_requires=['fabric', 'pyvmomi'],
+    install_requires=src_requirements(),
     packages=find_packages(),
     test_suite='nose.collector',
-    tests_require=['nose'],
+    tests_require=test_requirements(),
 )
