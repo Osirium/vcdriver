e4fac5590db9bcf2a022d27550a967d419e04221
Carlos <carlos.ruiz.lantero@gmail.com>
2017-01-04 11:53:26 +0000

    Fix setup.py weirdness

--- setup.py
+++ setup.py
@@ -1,33 +1,20 @@
-import os
 from setuptools import setup, find_packages
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
-def src_requirements():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
-
-def test_requirements():
-    with open(os.path.join('test', 'requirements.txt')) as f:
-        return f.read().splitlines()
-
-
 setup(
-    version='0.1.1',
+    version='1.0.0',
     name='vcdriver',
     description='Vcenter driver for testing purposes',
-    long_description=readme(),
+    long_description=(
+        "This is a vcenter driver, based on pyvmomi.\nAdding this extra layer "
+        "helps you drive your vcenter instance easier and it makes it a useful"
+        " testing tool.\nThe ssh utility uses fabric, so it's limited to what "
+        "fabric and ssh can do."
+    ),
     url='https://github.com/Lantero/vcdriver',
     author='Carlos Ruiz Lantero',
     author_email='carlos.ruiz.lantero@gmail.com',
     license='MIT',
-    install_requires=src_requirements(),
+    install_requires=['fabric', 'pyvmomi'],
     packages=find_packages(),
-    test_suite='nose.collector',
-    tests_require=test_requirements(),
 )
