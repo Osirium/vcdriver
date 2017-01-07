d62551c2c114af767808bfe33d9b6f953c36464b
Carlos <carlos.ruiz.lantero@gmail.com>
2017-01-07 17:23:41 +0000

    Add tests to cover 100%

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='1.0.0',
+    version='1.0.1',
     name='vcdriver',
     description='Vcenter driver for testing purposes',
     long_description=(
--- test/test_auth.py
+++ test/test_auth.py
@@ -1,7 +1,31 @@
+import mock
 import unittest
+import uuid
 
 from vcdriver import auth
 
 
+class ConnectionMock(object):
+    def __init__(self):
+        self.__setattr__('content', mock.MagicMock())
+        setattr(self.content, 'sessionManager', mock.MagicMock())
+        setattr(
+            self.content.sessionManager,
+            'currentSession',
+            mock.MagicMock()
+        )
+        setattr(
+            self.content.sessionManager.currentSession,
+            'key',
+            str(uuid.uuid4())
+        )
+
+
 class TestAuth(unittest.TestCase):
-    pass
+    @mock.patch('vcdriver.auth.connect.SmartConnect')
+    @mock.patch('vcdriver.auth.connect.Disconnect')
+    @mock.patch('vcdriver.auth.atexit.register')
+    def test_session(self, register, disconnect, connect):
+        auth.Session().close()
+        self.assertEqual(connect.call_count, 1)
+        self.assertEqual(disconnect.call_count, 1)
