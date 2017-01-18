689cbfdec9789fdfd941963a3cf7992b3611bd24
Carlos <carlos.ruiz.lantero@gmail.com>
2017-01-18 17:05:33 +0000

    Fix exception messages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='1.1.2',
+    version='1.1.3',
     name='vcdriver',
     description='Vcenter driver for testing purposes',
     long_description=(
--- vcdriver/exceptions.py
+++ vcdriver/exceptions.py
@@ -1,7 +1,6 @@
 class TooManyObjectsFound(Exception):
     def __init__(self, object_type, name):
-        super(TooManyObjectsFound, self).__init__()
-        self.message = (
+        super(TooManyObjectsFound, self).__init__(
             'Two or more objects of type {} with name "{}" were found'.format(
                 object_type, name
             )
@@ -10,8 +9,7 @@ class TooManyObjectsFound(Exception):
 
 class NoObjectFound(Exception):
     def __init__(self, object_type, name):
-        super(NoObjectFound, self).__init__()
-        self.message = (
+        super(NoObjectFound, self).__init__(
             'No objects of type {} with name "{}" were found'.format(
                 object_type, name
             )
@@ -20,25 +18,25 @@ class NoObjectFound(Exception):
 
 class TimeoutError(Exception):
     def __init__(self, description, timeout):
-        super(TimeoutError, self).__init__()
-        self.message = '"{}" timed out ({} secs)'.format(description, timeout)
+        super(TimeoutError, self).__init__(
+            '"{}" timed out ({} secs)'.format(description, timeout)
+        )
 
 
 class SshError(Exception):
     def __init__(self, command, return_code):
-        super(SshError, self).__init__()
-        self.message = (
+        super(SshError, self).__init__(
             '"{}" failed with exit code {}'.format(command, return_code)
         )
 
 
 class UploadError(Exception):
     def __init__(self, path):
-        super(UploadError, self).__init__()
-        self.message = self.message = 'Failed to upload "{}"'.format(path)
+        super(UploadError, self).__init__('Failed to upload "{}"'.format(path))
 
 
 class DownloadError(Exception):
     def __init__(self, path):
-        super(DownloadError, self).__init__()
-        self.message = self.message = 'Failed to download "{}"'.format(path)
+        super(DownloadError, self).__init__(
+            'Failed to download "{}"'.format(path)
+        )
