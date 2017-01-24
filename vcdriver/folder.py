ee23c03048b3e20b61358353ecbdc17386d23343
Carlos <carlos.ruiz.lantero@gmail.com>
2017-01-24 17:07:35 +0000

    Add timeout to folder.destroy_virtual_machines

--- vcdriver/folder.py
+++ vcdriver/folder.py
@@ -5,16 +5,21 @@ from vcdriver.helpers import get_vcenter_object
 from vcdriver.vm import VirtualMachine
 
 
-def destroy_virtual_machines(folder_name):
+def destroy_virtual_machines(folder_name, timeout=600):
     """
     Destroy all the virtual machines in the folder with the given name
     :param folder_name: The folder name
+    :param timeout: The timeout for vcenter tasks in seconds
+
+    :return: A list with the destroyed vms
     """
     folder = get_vcenter_object(Session().connection, vim.Folder, folder_name)
     destroyed_vms = []
     for entity in folder.childEntity:
         if isinstance(entity, vim.VirtualMachine):
-            vm = VirtualMachine(name=entity.summary.config.name)
+            vm = VirtualMachine(
+                name=entity.summary.config.name, timeout=timeout
+            )
             vm.__setattr__('_vm_object', entity)
             vm.destroy()
             destroyed_vms.append(vm)
