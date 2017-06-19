b75db84e40fee39c88124c1c856fdfa898bb6d7e
Carlos <carlos.ruiz.lantero@gmail.com>
2017-06-19 11:15:28 +0100

    Remove old config

--- vcdriver/config.py
+++ vcdriver/config.py
@@ -4,19 +4,6 @@ from six.moves import configparser
 
 from vcdriver.exceptions import MissingConfigValues
 
-# FIXME: Deprecated (Remove)
-HOST = os.getenv('VCDRIVER_HOST')
-PORT = os.getenv('VCDRIVER_PORT')
-USERNAME = os.getenv('VCDRIVER_USERNAME')
-PASSWORD = os.getenv('VCDRIVER_PASSWORD')
-RESOURCE_POOL = os.getenv('VCDRIVER_RESOURCE_POOL')
-DATA_STORE = os.getenv('VCDRIVER_DATA_STORE')
-FOLDER = os.getenv('VCDRIVER_FOLDER')
-VM_SSH_USERNAME = os.getenv('VCDRIVER_VM_SSH_USERNAME')
-VM_SSH_PASSWORD = os.getenv('VCDRIVER_VM_SSH_PASSWORD')
-VM_WINRM_USERNAME = os.getenv('VCDRIVER_VM_WINRM_USERNAME')
-VM_WINRM_PASSWORD = os.getenv('VCDRIVER_VM_WINRM_PASSWORD')
-# End of FIXME
 
 _config = {
     'Vsphere Session': {
