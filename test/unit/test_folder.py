8fb254bbc37b6c72ab4f7fda449f4a4436d03c9f
Carlos <carlos.ruiz.lantero@gmail.com>
2017-06-21 10:17:14 +0100

    Migrate folder tests to pytest

--- test/unit/test_folder.py
+++ test/unit/test_folder.py
@@ -1,5 +1,4 @@
 import mock
-import unittest
 
 from pyVmomi import vim
 
@@ -7,22 +6,20 @@ from vcdriver.folder import destroy_virtual_machines
 from vcdriver.vm import VirtualMachine
 
 
-class TestFolder(unittest.TestCase):
-    @mock.patch('vcdriver.vm.connection')
-    @mock.patch('vcdriver.folder.connection')
-    @mock.patch('vcdriver.folder.get_vcenter_object_by_name')
-    @mock.patch.object(VirtualMachine, 'destroy')
-    def test_destroy_virtual_machines(
-            self, destroy, get_vcenter_object_by_name, folder_connection,
-            vm_connection
-    ):
-        vm1 = mock.MagicMock(spec=vim.VirtualMachine)
-        vm1.summary.config.name = ''
-        vm2 = mock.MagicMock(spec=vim.VirtualMachine)
-        vm2.summary.config.name = ''
-        other = mock.MagicMock()
-        folder_mock = mock.MagicMock()
-        folder_mock.childEntity = [vm1, vm2, other]
-        get_vcenter_object_by_name.return_value = folder_mock
-        self.assertEqual(len(destroy_virtual_machines('wrong folder')), 2)
-        self.assertEqual(destroy.call_count, 2)
+@mock.patch('vcdriver.vm.connection')
+@mock.patch('vcdriver.folder.connection')
+@mock.patch('vcdriver.folder.get_vcenter_object_by_name')
+@mock.patch.object(VirtualMachine, 'destroy')
+def test_destroy_virtual_machines(
+        destroy, get_vcenter_object_by_name, folder_connection, vm_connection
+):
+    vm1 = mock.MagicMock(spec=vim.VirtualMachine)
+    vm1.summary.config.name = ''
+    vm2 = mock.MagicMock(spec=vim.VirtualMachine)
+    vm2.summary.config.name = ''
+    other = mock.MagicMock()
+    folder_mock = mock.MagicMock()
+    folder_mock.childEntity = [vm1, vm2, other]
+    get_vcenter_object_by_name.return_value = folder_mock
+    assert len(destroy_virtual_machines('wrong folder')) == 2
+    assert destroy.call_count == 2
