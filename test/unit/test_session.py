7f27b5172c5411e543e630aeabce3a057786b501
Carlos <carlos.ruiz.lantero@gmail.com>
2017-06-21 10:04:09 +0100

    Migrate session tests to pytest

--- test/unit/test_session.py
+++ test/unit/test_session.py
@@ -1,23 +1,21 @@
 import mock
-import unittest
 
 from vcdriver.session import connection, close, id
 
 
-class TestAuth(unittest.TestCase):
-    @mock.patch('vcdriver.session.SmartConnect')
-    @mock.patch('vcdriver.session.Disconnect')
-    def test_session(self, disconnect, connect):
-        connection(
-            vcdriver_username='something', vcdriver_password='something',
-            vcdriver_host='something', vcdriver_port='something'
-        )
-        connection(
-            vcdriver_username='something', vcdriver_password='something',
-            vcdriver_host='something', vcdriver_port='something'
-        )
-        id()
-        close()
-        close()
-        self.assertEqual(connect.call_count, 1)
-        self.assertEqual(disconnect.call_count, 1)
+@mock.patch('vcdriver.session.SmartConnect')
+@mock.patch('vcdriver.session.Disconnect')
+def test_session(disconnect, connect):
+    connection(
+        vcdriver_username='something', vcdriver_password='something',
+        vcdriver_host='something', vcdriver_port='something'
+    )
+    connection(
+        vcdriver_username='something', vcdriver_password='something',
+        vcdriver_host='something', vcdriver_port='something'
+    )
+    id()
+    close()
+    close()
+    assert connect.call_count == 1
+    assert disconnect.call_count == 1
