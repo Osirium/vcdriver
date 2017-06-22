6023924496b7b1ff44bf4cfd8a92f899db9fc4b9
Carlos <carlos.ruiz.lantero@gmail.com>
2017-06-22 17:27:55 +0100

    Bump version and update CHANGELOG

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='3.0.3',
+    version='3.1.0',
     name='vcdriver',
     description='A vcenter driver based on pyvmomi, fabric and pywinrm',
     url='https://github.com/Lantero/vcdriver',
