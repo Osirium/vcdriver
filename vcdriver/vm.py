a22aa4032f5806087255f1ca4a3caed279b5d95b
Carlos <carlos.ruiz.lantero@gmail.com>
2017-09-22 16:34:21 +0100

    Improve winrm_upload function and fix tests

--- vcdriver/vm.py
+++ vcdriver/vm.py
@@ -1,6 +1,9 @@
+from __future__ import print_function
+
 import base64
 import contextlib
 import os
+import sys
 import uuid
 
 from colorama import Style, Fore
@@ -28,7 +31,7 @@ from vcdriver.helpers import (
     wait_for_vcenter_task,
     fabric_context,
     check_ssh_service,
-    check_winrm_service
+    check_winrm_service,
 )
 
 
@@ -367,28 +370,48 @@ class VirtualMachine(object):
                     kwargs['vcdriver_vm_winrm_username'],
                     kwargs['vcdriver_vm_winrm_password'],
                 ),
-                read_timeout_sec=self.timeout+1,
+                read_timeout_sec=self.timeout + 1,
                 operation_timeout_sec=self.timeout,
                 **winrm_kwargs
             )
             with open(local_path, 'rb') as f:
                 contents = f.read()
-            for i in range(0, len(contents), step):
-                winrm_session.run_ps(
-                    """
-    $filePath = "{}"
-    $s = @"
-    {}
-    "@
-    $data = [System.Convert]::FromBase64String($s)
-    add-content -value $data -encoding byte -path $filePath
-                    """.format(
-                        remote_path, base64.b64encode(contents[i:i+step])
+                size = len(contents)
+            winrm_session.run_ps(
+                'if (Test-Path {0}) {{ Remove-Item {0} }}'.format(remote_path)
+            )
+            for i in range(0, size, step):
+                script = (
+                    'add-content -value '
+                    '$([System.Convert]::FromBase64String("{}")) '
+                    '-encoding byte -path {}'.format(
+                        base64.b64encode(contents[i:i + step]),
+                        remote_path
                     )
                 )
-            print('{} -> {} WINRM transfer successful'.format(
-                local_path, remote_path
-            ))
+                result = winrm_session.run_ps(script)
+                status_code = result.status_code
+                if status_code != 0:
+                    raise WinRmError(script, status_code)
+                transferred = i + step
+                if transferred > size:
+                    transferred = size
+                progress_blocks = transferred * 30 // size
+                percentage_string = str((100 * transferred) // size) + ' %'
+                percentage_string = (
+                    ' ' * (5 - len(percentage_string)) + percentage_string
+                )
+                print(
+                    '\r{} ... [{}{}] {}'.format(
+                        'Copying "{}" to "{}"'.format(local_path, remote_path),
+                        '=' * progress_blocks,
+                        ' ' * (30 - progress_blocks),
+                        percentage_string
+                    ),
+                    end=''
+                )
+                sys.stdout.flush()
+            print('')
 
     def find_snapshot(self, name):
         """
