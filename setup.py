7689c4f475d621c52cd62676c9d38709f8a3c622
Thomas Grainger <tagrain@gmail.com>
2019-02-18 17:11:56 +0000

    bump to 5.0.0, rename back to vcdriver

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='0.0.0',
-    name='vcdriver2',
+    version='5.0.0',
+    name='vcdriver',
     description='A vcenter driver based on pyvmomi, fabric and pywinrm',
     url='https://github.com/Osirium/vcdriver',
     license='MIT',
